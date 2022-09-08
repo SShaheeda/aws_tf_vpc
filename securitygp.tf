60e40e0d48e64cc97253bc5d8a72817cb3877be5
shaikis <shaik.urs@gmail.com>
2022-09-08 21:33:19 +0530

    added vpc changes

---
+++ securitygp.tf
@@ -0,0 +1,24 @@
+resource "aws_security_group" "mysgp" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = aws_vpc.myvpc.id
+
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["103.82.209.133/32"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
